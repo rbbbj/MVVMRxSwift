c825855a3f9e3b77a74aecfc44a598c775a44855
Robert <robertasbaronas@hotmail.co>
2019-02-26 20:29:41 +0200

    cleanup ApplicationCoordinator

--- MVVMRxSwift/Coordinators/ApplicationCoordinator.swift
+++ MVVMRxSwift/Coordinators/ApplicationCoordinator.swift
@@ -3,18 +3,18 @@ import UIKit
 class ApplicationCoordinator: Coordinator {
     let window: UIWindow
     let navigationController: UINavigationController
-    let albumsListCoordinator: AlbumsListCoordinator
     var childCoordinators: [Coordinator] = []
     
     init(window: UIWindow) {
         self.window = window
         navigationController = UINavigationController()
         navigationController.navigationBar.prefersLargeTitles = true
-        albumsListCoordinator = AlbumsListCoordinator(navigationController: navigationController)
     }
     
     func start() {
         window.rootViewController = navigationController
+        let albumsListCoordinator = AlbumsListCoordinator(navigationController: navigationController)
+        addChildCoordinator(albumsListCoordinator)
         albumsListCoordinator.start()
         window.makeKeyAndVisible()
     }
